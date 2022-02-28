f08d9fd42c3804d1dd25763f32acbe5d3ecba3be
slasher <mcq.sejust@gmail.com>
2022-02-28 15:36:18 +0800

    style(tinker): benckmark of volume cache
    Signed-off-by: slasher <mcq.sejust@gmail.com>

--- tinker/volume_cache_test.go
+++ tinker/volume_cache_test.go
@@ -25,7 +25,7 @@ import (
 	"github.com/cubefs/blobstore/tinker/client"
 )
 
-func TestVolumeCache(t *testing.T) {
+func TestTinkerVolumeCache(t *testing.T) {
 	cmClient := NewMockClusterMgrAPI(gomock.NewController(t))
 	cmClient.EXPECT().ListVolume(any, any, any).Times(2).DoAndReturn(
 		func(_ context.Context, marker proto.Vid, _ int) ([]client.VolInfo, proto.Vid, error) {
@@ -56,15 +56,41 @@ func TestVolumeCache(t *testing.T) {
 	_, err = volCache.Update(1)
 	require.ErrorIs(t, err, ErrFrequentlyUpdate)
 
-	// update failed
+	// list and get failed
+	cmClient.EXPECT().ListVolume(any, any, any).AnyTimes().Return(nil, proto.Vid(0), errMock)
 	cmClient.EXPECT().GetVolInfo(any, any).Return(client.VolInfo{}, errMock)
 	volCache = NewVolumeCache(cmClient, -1)
 	_, err = volCache.Get(1)
 	require.ErrorIs(t, err, errMock)
-
-	// list volume failed
-	cmClient.EXPECT().ListVolume(any, any, any).AnyTimes().Return(nil, proto.Vid(0), errMock)
-	volCache = NewVolumeCache(cmClient, -1)
 	err = volCache.Load()
 	require.ErrorIs(t, err, errMock)
+}
+
+func BenchmarkTinkerVolumeCache(b *testing.B) {
+	for _, cs := range []struct {
+		Name  string
+		Items int
+	}{
+		{"tiny", 1 << 5},
+		{"norm", 1 << 16},
+		{"huge", 1 << 20},
+	} {
+		items := cs.Items
+		b.Run(cs.Name, func(b *testing.B) {
+			vols := make([]client.VolInfo, items)
+			for idx := range vols {
+				vols[idx] = client.VolInfo{Vid: proto.Vid(idx)}
+			}
+			cmCli := NewMockClusterMgrAPI(gomock.NewController(b))
+			cmCli.EXPECT().ListVolume(any, any, any).Return(vols, defaultMarker, nil)
+
+			cacher := NewVolumeCache(cmCli, -1)
+			require.NoError(b, cacher.Load())
+
+			b.ResetTimer()
+			for ii := 0; ii < b.N; ii++ {
+				cacher.Get(proto.Vid(ii % items))
+			}
+		})
+	}
 }
